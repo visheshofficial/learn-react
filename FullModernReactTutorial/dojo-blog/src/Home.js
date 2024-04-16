87b96821e6b0ae0870f82b45d48a983453cf8ee1
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-04-16 21:12:17 +0100

    Full React Tutorial #8 - Using State (useState hook)

--- FullModernReactTutorial/dojo-blog/src/Home.js
+++ FullModernReactTutorial/dojo-blog/src/Home.js
@@ -1,16 +1,33 @@
+import { useState } from 'react'
 const Home = () => {
+  // let name = 'Alice'
+  const [reactName, setReactName] = useState('Alice')
+  const [age, setAge] = useState(25)
+
   const hancleClick = (e) => {
-    console.log(e)
-    console.log('Hello there!')
+    // console.log(e)
+    // name = 'Bob'
+    if (reactName === 'Bob') {
+      setReactName('Alice')
+      setAge(age + 1)
+    } else {
+      setReactName('Bob')
+      setAge(age + 2)
+    }
+
+    console.log('Hello there!' + reactName)
   }
 
   const hancleClickAgain = (name, event) => {
     console.log('Hello ' + name + '!')
-    console.log(event)
+    // console.log(event)
   }
   return (
     <div className="home">
       <h2>Home page</h2>
+      <p>
+        {reactName} is {age} old
+      </p>
       <button onClick={hancleClick}>Click me!</button>
       <button onClick={(e) => hancleClickAgain('Alice', e)}>
         Click me again!
