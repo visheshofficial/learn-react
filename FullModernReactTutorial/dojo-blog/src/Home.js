6fe73849161d2893d734f0741502d4f6c42ad8a9
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-04-16 20:36:08 +0100

    Full React Tutorial #7 - Click Events

--- FullModernReactTutorial/dojo-blog/src/Home.js
+++ FullModernReactTutorial/dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+  const hancleClick = (e) => {
+    console.log(e)
+    console.log('Hello there!')
+  }
+
+  const hancleClickAgain = (name, event) => {
+    console.log('Hello ' + name + '!')
+    console.log(event)
+  }
   return (
     <div className="home">
       <h2>Home page</h2>
+      <button onClick={hancleClick}>Click me!</button>
+      <button onClick={(e) => hancleClickAgain('Alice', e)}>
+        Click me again!
+      </button>
     </div>
   )
 }
