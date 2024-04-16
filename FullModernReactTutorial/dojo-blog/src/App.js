9f4d386903c22f0797b393060889c8d154e64eca
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-04-16 17:48:31 +0100

    Lesson 4 Full React Tutoria

--- FullModernReactTutorial/dojo-blog/src/App.js
+++ FullModernReactTutorial/dojo-blog/src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
+import './App.css'
 
 function App() {
+  const title = 'Welcome to new Dojo blog'
+  const likes = 50
+  const google_site = 'www.google.com'
   return (
     <div className="App">
       <div className="content">
-        <h1>Vishesh App Content</h1>
-      </div>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
 
+        <p>{10}</p>
+        <p>{10 + 10}</p>
+        <p>{'hello, world'}</p>
+        <p>{['this ', 'is ', 'an ', 'array']}</p>
+
+        <a href={google_site}>Google site</a>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
