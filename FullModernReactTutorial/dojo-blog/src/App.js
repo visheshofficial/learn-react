3d643c354797df0f495ff4ee12d16481e89394d6
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-04-16 18:42:11 +0100

    Lesson 5 Full React Tutoria

--- FullModernReactTutorial/dojo-blog/src/App.js
+++ FullModernReactTutorial/dojo-blog/src/App.js
@@ -1,21 +1,15 @@
 import './App.css'
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   const title = 'Welcome to new Dojo blog'
-  const likes = 50
-  const google_site = 'www.google.com'
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-
-        <p>{10}</p>
-        <p>{10 + 10}</p>
-        <p>{'hello, world'}</p>
-        <p>{['this ', 'is ', 'an ', 'array']}</p>
-
-        <a href={google_site}>Google site</a>
+        <Home />
       </div>
     </div>
   )
