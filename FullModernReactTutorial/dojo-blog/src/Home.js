1fcf7b056141fa1135f91d6b0415e929527bcffc
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-04-19 15:19:51 +0100

    Full React Tutorial #11 - Props

--- FullModernReactTutorial/dojo-blog/src/Home.js
+++ FullModernReactTutorial/dojo-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import BlogList from './BlogList'
 const Home = () => {
   // let name = 'Alice'
   const [reactName, setReactName] = useState('Alice')
@@ -55,12 +56,7 @@ const Home = () => {
         Click me again!
       </button>
 
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All blogs" />
     </div>
   )
 }
