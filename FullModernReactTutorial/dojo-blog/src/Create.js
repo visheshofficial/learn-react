7e651faf8468c2e24a9adf67d397f3f73b1b0cce
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-04-23 14:57:25 +0100

    Full React Tutorial #27 - Controlled Inputs (forms)

--- FullModernReactTutorial/dojo-blog/src/Create.js
+++ FullModernReactTutorial/dojo-blog/src/Create.js
@@ -1,7 +1,36 @@
+import { useState } from 'react'
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('alice')
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="alice">Alice</option>
+          <option value="bob">Bob</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+      <p>Title: {title}</p>
+      <p>Body: {body}</p>
+      <p>Author: {author}</p>
     </div>
   )
 }
