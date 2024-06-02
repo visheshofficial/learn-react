64b07c160872bcff2c77d6153d12cc1449830d5e
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-06-02 12:42:26 +0100

    03-components-jsx-props

--- Maximilian-Schwarzmüller-Course/PracticeE2/03-components-jsx-props/src/App.jsx
+++ Maximilian-Schwarzmüller-Course/PracticeE2/03-components-jsx-props/src/App.jsx
@@ -1,26 +1,77 @@
-import './App.css'
+import { useState } from 'react'
 
-function Header(){
-  return<header>
-  <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-  <h1>React Essentials</h1>
-  <p>
-    Fundamental React concepts you will need for almost any app you are
-    going to build!
-  </p>
-</header>
-}
+import { CORE_CONCEPTS } from './data.js'
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx'
+import TabButton from './components/TabButton.jsx'
+import { EXAMPLES } from './data.js'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [selectedTopic, setSelectedTopic] = useState()
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton)
+    console.log(selectedTopic)
+  }
+
+  const tabContent = selectedTopic ? (
+    <>
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </>
+  ) : (
+    <p>Please select a topic.</p>
+  )
+  console.log('APP COMPONENT EXECUTING')
 
   return (
-    <>
-    <Header/>
-    <main>
-        <h2>Time to get started!</h2>
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept key={index} {...concept} />
+            ))}
+
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {Object.keys(EXAMPLES).map((example) => (
+              <TabButton
+                isSelected={selectedTopic === example}
+                key={example}
+                onSelect={() => handleSelect(example)}
+              >
+                {EXAMPLES[example].title}
+              </TabButton>
+            ))}
+
+            {/* <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton> */}
+          </menu>
+          <div id="tab-content">{tabContent}</div>
+        </section>
       </main>
-    </>
+    </div>
   )
 }
 
--- Maximilian-Schwarzmüller-Course/PracticeE2/03-components-jsx-props/src/index.jsx
+++ Maximilian-Schwarzmüller-Course/PracticeE2/03-components-jsx-props/src/index.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
