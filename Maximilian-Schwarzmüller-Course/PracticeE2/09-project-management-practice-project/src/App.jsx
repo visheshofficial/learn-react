4c6e82277b8a8f00b54f391edbd1b5903c23b26e
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2024-07-05 19:42:43 +0100

    in progress 09-project-management-practice-project

---
+++ Maximilian-Schwarzmüller-Course/PracticeE2/09-project-management-practice-project/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSideBar from './components/ProjectsSideBar'
+
+function App() {
+  return (
+    <main>
+      <ProjectSideBar />
+    </main>
+  )
+}
+
+export default App
---
+++ Maximilian-Schwarzmüller-Course/PracticeE2/09-project-management-practice-project/src/components/ProjectsSideBar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectSideBar() {
+  return (
+    <aside>
+      <h2>Your projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul>P1</ul>
+    </aside>
+  )
+}
